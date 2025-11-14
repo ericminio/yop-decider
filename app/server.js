316b6457d88a3df2ef0d0a08d397bdfb3fc3ce19
Eric Mignot <eric.mignot@gmail.com>
2025-11-13 23:33:06 -0800

    magic number in endpoint

--- app/server.js
+++ app/server.js
@@ -1,6 +1,16 @@
 import { RouteDefault, Router, Server, html } from "../yop/index.js";
 
 const router = new Router([
+  {
+    matches: (incoming) => incoming.url.startsWith("/propositions"),
+    go: (_, response) => {
+      const propositions = [
+        { text: "I propose we start today", owner: "Charlie" },
+      ];
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.end(JSON.stringify({ propositions }));
+    },
+  },
   new RouteDefault(html(new URL("./index.html", import.meta.url))),
 ]);
 
