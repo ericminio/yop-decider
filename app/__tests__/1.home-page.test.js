c8b9b8203d3b91bfc551b6d9c1bb85a6e725ea17
Eric Mignot <eric.mignot@gmail.com>
2025-11-19 14:20:37 -0800

    HP displays existing propositions

--- app/__tests__/1.home-page.test.js
+++ app/__tests__/1.home-page.test.js
@@ -10,13 +10,21 @@ describe("home page", server, (page) => {
   before(async () => {
     const charlie = new User({ name: "Charlie" }, server.bus);
     charlie.proposes("I propose we start today");
+    const dana = new User({ name: "Dana" }, server.bus);
+    dana.proposes("I propose we advance the launch");
   });
 
-  test("displays existing proposition", async () => {
+  test("displays existing propositions", async () => {
     await eventually(page, async () => {
       assert.match(
         await page.section("News"),
         /Charlie.*I propose we start today/,
+      );
+    });
+    await eventually(page, async () => {
+      assert.match(
+        await page.section("News"),
+        /Dana.*I propose we advance the launch/,
       );
     });
   });
